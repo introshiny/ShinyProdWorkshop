4cd5a1481395fc7b7e851dd6678ecc39af45bf7f
GuidoMaggio <guido.maggio@mirai-solutions.com>
2022-09-16 14:55:38 +0200

    deploy github action

---
+++ deploy/deploy-shinyapps.R
@@ -0,0 +1,6 @@
+rsconnect::setAccountInfo(
+  Sys.getenv("SHINYAPPS_ACCOUNT"),
+  Sys.getenv("SHINYAPPS_TOKEN"),
+  Sys.getenv("SHINYAPPS_SECRET")
+)
+rsconnect::deployApp(appName = "ShinyProdWorkshop")
